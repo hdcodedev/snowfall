bd3b298b7c5f14bd12be5b0de876f7e61e338705
HDCode <hdcodedev@gmail.com>
2025-12-22 21:10:05 +0100

    fix: update version to 1.0.2 in package.json and simplify entry point in tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/**/*.ts', 'src/**/*.tsx'],
+    entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
     dts: true,
     sourcemap: true,
     clean: true,
     minify: false,
     external: ['react', 'react-dom'],
-    bundle: false,
 });
